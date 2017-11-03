599311e129cb00cc477c300e4e45fb0713d3de8e
Andriy Vasylyk <deadsith@hotmail.com>
2017-11-03 21:55:40 +0200

    Reimplemented config

--- spring_lab/src/main/java/com/sith/spring_lab/AppInitializer.java
+++ spring_lab/src/main/java/com/sith/spring_lab/AppInitializer.java
@@ -1,19 +1,23 @@
 package com.sith.spring_lab;
-import org.springframework.web.*;
-import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.context.support.XmlWebApplicationContext;
+import com.sith.spring_lab.config.AppConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
-public class AppInitializer implements WebApplicationInitializer{
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
     @Override
-    public void onStartup(javax.servlet.ServletContext container) throws ServletException {
-        XmlWebApplicationContext context = new XmlWebApplicationContext();
-        context.setConfigLocation("spring.xml");
-        ServletRegistration.Dynamic dispatcher = container
-                .addServlet("dispatcher", new DispatcherServlet(context));
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { AppConfig.class };
     }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
 }
